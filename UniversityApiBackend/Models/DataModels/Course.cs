e76e1527274b1231e7072bb439b2d267d6ecff44
Lluis Bonet <lluis.bonet@convidad.es>
2022-08-14 15:15:04 +0200

    exercice 1

---
+++ UniversityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Course : BaseEntity
+    {
+        [Required, MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [MaxLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+        public string LongDescription { get; set; } = string.Empty;
+        public string TargetAudience { get; set; } = string.Empty;
+        public string Goals { get; set; } = string.Empty;
+        public string Requisits { get; set; } = string.Empty;
+        public Level Level { get; set; }
+    }
+
+    public enum Level
+    {
+        Basic = 1,
+        Intermediate = 2,
+        Advanced = 3,
+    }
+}
---
+++ UniversityApiBackend/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Connection with SQL Server Express
+const string CONNECTIONNAME = "UniversityDB";
+var connectionString = builder.Configuration.GetConnectionString(CONNECTIONNAME);
+
+// Add context
+builder.Services.AddDbContext<UniversityDBContext>(options => options.UseSqlServer(connectionString));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
